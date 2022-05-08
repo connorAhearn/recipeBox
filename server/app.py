b266f2bf14b36b459b67980b383be42585889a29
Connor Ahearn <connor.ahearn305@gmail.com>
2022-05-08 12:57:17 -0400

    Connected Spoonacular API call to Flask server, initialized basic React app

--- server/app.py
+++ server/app.py
@@ -1,7 +1,24 @@
 from flask import Flask
 
+import sys
+
+# Allows us to import api from another folder
+sys.path.insert(1, '../spoonacular')
+from api import get_ingredients
+
 app = Flask(__name__)
 
+# Homepage route
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return 'hello world'
+
+# Basic API call for getting ingredients from a provided URL
+# TODO: Take a provided url as input & insert into the get_ingredients function
+@app.route("/ingredients")
+def ingredients():
+    return {'ingredients': get_ingredients('https://www.sipandfeast.com/pasta-alla-norcina/')}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
